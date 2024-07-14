2d9a2baa152c7ea1d287b7eed57dcb6a4dfd11f0
salahbm <salahbm.001@gmail.com>
2024-07-15 00:16:36 +0900

    first commit

---
+++ app/(site)/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title> Avicenna Medical Travel</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Get started with Avicenna" />
+      <link rel="icon" href="/images/favicon.ico" />
+    </>
+  );
+}
