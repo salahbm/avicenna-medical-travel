f2b146b1c8b901ace1ff7c6aa1ee73ddcc10ed7c
salahbm <salahbm.001@gmail.com>
2024-08-04 11:19:13 +0900

    [add] metadata

--- src/app/[locale]/(site)/layout.tsx
+++ src/app/[locale]/(site)/layout.tsx
@@ -2,7 +2,9 @@ import { Inter } from "next/font/google";
 import "../../globals.css";
 import RootProvider from "../../context/root-provider";
 import TranslationsProvider from "../../context/TranslationsProvider";
-import { unstable_setRequestLocale } from "next-intl/server";
+import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
+import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import { Metadata } from "next";
 const inter = Inter({ subsets: ["latin"] });
 
 // Can be imported from a shared config
@@ -11,7 +13,34 @@ const locales = ["en", "ru", "uz"];
 export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
 }
+export async function generateMetadata({
+  params: { locale },
+}: Params): Promise<Metadata> {
+  const t = await getTranslations({ locale, namespace: "Meta" });
+  const title = t("home.title", { brand: "Avicenna Medical Travel" });
+  const description = t("home.description", {
+    brand: "Avicenna Medical Travel",
+  });
 
+  return {
+    title,
+    description,
+    metadataBase: new URL(`https://avicennamedical.vercel.app`),
+    alternates: {
+      canonical: `/${locale}`,
+      languages: {
+        en: "/en",
+      },
+    },
+    openGraph: {
+      title,
+      description,
+      url: `/`,
+      siteName: "Avicenna Medical Travel",
+      type: "website",
+    },
+  };
+}
 export default async function RootLayout({
   children,
   params: { locale },
